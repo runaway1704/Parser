cb13c62bedd057825081c59e7b152326555107be
Andrey Lukyanchuk <andreylukyanchuk623@gmail.com>
2020-07-22 13:29:20 +0300

    added not_needed_items

--- parser.py
+++ parser.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-
-lowest_price = str(input("low price: "))
-highest_price = str(input("max price: "))
+lowest_price = "1500"
+highest_price = "2000"
+# lowest_price = str(input("low price: "))
+# highest_price = str(input("max price: "))
 url = f"https://market.csgo.com/?s=pop&r=&q=&rs={lowest_price};{highest_price}&h="
 headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
@@ -18,6 +19,9 @@ def get_html(url):
 def get_content(html):
     soup = BeautifulSoup(html, "html.parser")
     items = soup.find_all("a", class_="item hot")
+    not_needed_items = ("Сувенирный", "StatTrak™", "Souvenir", "AK-47 | Черный глянец",
+                      "AK-47 | Black Laminate (Field-Tested)",
+                      "AWP | Электрический улей (Прямо с завода)",)
     container = []
     for item in items:
         container.append({
@@ -27,11 +31,12 @@ def get_content(html):
         })
 
     for items in container:
-        for key, value in items.items():
-            print(key + " -> " + value)
-        print("---------------------------------------------" * 3)
-
-    time.sleep(360)
+        if not any(i in items["name"] for i in not_need_items):
+            for key, value in items.items():
+                print(key + " -> " + value)
+            print("---------------------------------------------" * 3)
+    #
+    time.sleep(10)
 
 
 def parse():
